79bc14158015cae7ba087874eb1a03061d16f5e7
Robert Alonso <robert.alonso.ac@gmail.com>
2017-06-15 22:29:28 -0500

    Update python client event

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from socketIO_client_nexus import SocketIO, LoggingNamespace
 
 with SocketIO('http://localhost', 3000, LoggingNamespace) as socketIO:
-    socketIO.emit('chat message', 'greetings from python')
-    socketIO.wait(seconds=1)
+    socketIO.emit('venta')
+    
