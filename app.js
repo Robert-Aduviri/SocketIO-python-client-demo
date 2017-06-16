8b9a8e086dc38fe02ddc45bf094a6f7a1042cd94
Robert Alonso <robert.alonso.ac@gmail.com>
2017-06-15 22:14:43 -0500

    Add d3.js pie chart integrated with socket io

--- app.js
+++ app.js
@@ -2,6 +2,15 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var dataset = [
+  { label: 'Menus vendidos', count: 10 },
+  { label: 'Menus por vender', count: 90 }
+];
+
+app.get('/dataset', function(req, res){
+  res.send(JSON.stringify(dataset));
+});
+
 app.get('/', function(req, res){
 	res.sendFile(__dirname + '/index.html');
 });
@@ -12,10 +21,19 @@ app.get('/styles.css', function(req, res){
 
 io.on('connection', function(socket){
   console.log('a user connected');
+
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
     console.log('message: ' + msg);
   });
+
+  socket.on('venta', function(msg){
+    dataset[0].count++;
+    dataset[1].count--;
+    io.emit('venta', dataset);
+    console.log('Menus vendidos: ', dataset[0].count);
+  });
+
   socket.on('disconnect', function(){
     console.log('user disconnected');
   });
